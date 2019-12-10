604f24688ea2560697e1a7d6c77a5b3e01a47c96
garlandxie <garland.xie@gmail.com>
2019-12-09 21:58:28 -0500

    Added 500m spatial scale analysis

--- src/rlq.R
+++ src/rlq.R
@@ -25,7 +25,7 @@ met_500 <- read_csv(here("data/original", "land_use_metrics_500.csv"))
 # trait matrix of 31 species (excluded kleptoparasites)
 trait <- read_csv(here("data/final", "trait_matrix.csv"))
 
-# data - cleaning: comm matrix -------------------------------------------------
+# Clean: comm matrix -------------------------------------------------
 
 # Change taxon names to abbreviated versions 
 # plotting purposes
@@ -58,7 +58,7 @@ comm_tidy <- comm %>%
   # remove kleptoparasites
   select(-c("Ste_ver", "Coe_say", "Coe_alt", "Coe_moe"))
 
-# data - cleaning: trait matrix ------------------------------------------------
+# Clean: trait matrix ------------------------------------------------
 
 trait_clean <- trait %>%
   select(-"X1") %>%
@@ -83,7 +83,7 @@ trait_clean <- trait %>%
 # double-check
 str(trait_clean)
 
-# data cleaning - environmental variables -------------------------------------------
+# Clean: environmental vars ----------------------------------------------------
 
 # Re-order both environmental data matrices based on rows 
 # Makes it easy to do value-matching 
@@ -97,31 +97,88 @@ met_500_clean <- met_500 %>%
 
 # RLQ: 250 ---------------------------------------------------------------------
 
+# Procedure of RLQ follows closely to:
+# Dray et al. 2014. Ecology. 
+# https://doi.org/10.1890/13-0196.1
+# Supplementary Info 1
+
 # correspondance analysis 
 # community data matrix
 dudiL_250 <- comm_tidy %>%
   inner_join(met_250_clean, by = "ID") %>%
-  select(colnames(comm_tidy), -"ID") %>%
+  select(colnames(comm_tidy)) %>%
+  tibble::column_to_rownames(var = "ID") %>%
   dudi.coa(scannf = F)
 
 # principal component analysis
 # environmental variables
+# weighted by site weights from previous correspondance analysis
+
 dudiR_250 <- met_250_clean %>%
+  tibble::column_to_rownames(var = "ID") %>%
   select("250_urban" = "total.area_250_urban",
          "250_grass" = "total.area_250_grass",
          "250_tree"  = "total.area_250_tree_canopy") %>%
   dudi.pca(row.w = dudiL_250$lw, scannf = F)
 
-# hill-smith
+# hill-smith method: PCA of discrete/continuous data 
+# reference: Hill and Smith. 1976. Taxon
+# weighted by sites weight from previous correspondance analysis
 
 dudiQ_250 <- trait_clean %>%
-  select(-c(ID, spp)) %>%
+  select(-spp) %>%
+  tibble::column_to_rownames(var = "ID") %>%
   dudi.hillsmith(row.w = dudiL_250$cw, 
                  scannf = F)
 
-# RLQ
+# RLQ: 250m spatial scale 
+# obtain first two RLQ axes in a non-iteractive manner 
 RLQ_250 <- rlq(dudiR = dudiR_250, 
-           dudiQ = dudiQ_250, 
-           dudiL = dudiL_250)
+               dudiQ = dudiQ_250, 
+               dudiL = dudiL_250,
+               scannf = FALSE, nf = 2)
 
+# cumulative projected inertia (%)
+summary(RLQ_250)
 
+# RLQ: 500 ---------------------------------------------------------------------
+
+# L table - species abundance
+# correspondance analysis 
+# community data matrix
+dudiL_500 <- comm_tidy %>%
+  inner_join(met_500_clean, by = "ID") %>%
+  select(colnames(comm_tidy)) %>%
+  tibble::column_to_rownames(var = "ID") %>%
+  dudi.coa(scannf = F)
+
+# Q table - traits
+# hill-smith method: PCA of discrete/continuous data 
+# weighted by sites weight from previous correspondance analysis
+dudiQ_500 <- trait_clean %>%
+  select(-spp) %>%
+  tibble::column_to_rownames(var = "ID") %>%
+  dudi.hillsmith(row.w = dudiL_500$cw, 
+                 scannf = F)
+
+# R table - env vars
+# principal component analysis
+# weighted by site weights from previous correspondance analysis
+dudiR_500 <- met_500_clean %>%
+  tibble::column_to_rownames(var = "ID") %>%
+  select("500_urban" = "total.area_500_urban",
+         "500_grass" = "total.area_500_grass",
+         "500_tree"  = "total.area_500_tree_canopy") %>%
+  dudi.pca(row.w = dudiL_500$lw, scannf = F)
+
+# RLQ - 500
+# obtain first two RLQ axes in an non-iteractive manner 
+RLQ_500 <- rlq(dudiR = dudiR_500, 
+               dudiQ = dudiQ_500, 
+               dudiL = dudiL_500,
+               scannf = FALSE, nf = 2)
+
+# cumulative projected inertia (%)
+summary(RLQ_500)
+
+# plots ------------------------------------------------------------------------
